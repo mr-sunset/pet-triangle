9ec6f7df211d0d3a553132a7648592649d47c9eb
Jonathan Charette <jacharette@icloud.com>
2026-07-07 22:13:08 -0400

    Fixed styles, lay JS foundation

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const feedBtn = document.getElementById('feed');
+    const drinkBtn = document.getElementById('drink');
+    const playBtn = document.getElementById('play');
+
+    const foodCount = document.getElementById('food-count');
+    const drinkCount = document.getElementById('drink-count');
+    const playCount = document.getElementById('play-count');
+
+    const petColor = document.getElementById('pet-color');
+    const petName = document.getElementById('pet-name');
+
+    
+})
